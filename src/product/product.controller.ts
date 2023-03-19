6258dd39445d8c75cce621ddb73603969e810237
sebasromero <sebastian_eromero@hotmail.com>
2023-03-19 16:34:56 -0400

    At product.controller was added the http methods createProduct, getProducts and getProduct

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,35 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post, Res } from '@nestjs/common';
 import { CreateProductDTO } from "./dto/product.dto";
+import { ProductService } from "./product.service";
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
 
+    constructor( private productService: ProductService){} 
+
     @Post('/create')
-    createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+    async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+        const product = await this.productService.createProduct(createProductDTO)
         return res.status(HttpStatus.OK).json({
-            message: "received"
-        })
+            message: "Product Succesfully Created",
+            product
+        });
     }
 
+    @Get('/')
+    async getProducts(@Res() res) {
+        const products = await this.productService.getProducts();
+        return res.status(HttpStatus.OK).json({
+            products
+        })    
+    }
+
+    @Get('/:productID')
+    async getProduct(@Res() res, @Param('productID') productID){
+        const product = await this.productService.getProduct(productID)
+        if (!product) {
+            throw new NotFoundException('Product does not exists')
+        }
+        return res.status(HttpStatus.OK).json(product)
+    }
 }
